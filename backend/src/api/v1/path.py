93f8a5202dfc545049d724febeb7fe53ccc249d8
Chayut Sawang <43782805+ibosszzz@users.noreply.github.com>
2018-11-28 04:16:36 +0700

    add new format

--- backend/src/api/v1/path.py
+++ backend/src/api/v1/path.py
@@ -11,5 +11,14 @@ class PathView(HTTPMethodView):
         src, dst = src_dst.split(',')
         path_finder = PathFinder()
         paths = path_finder.all_by_manage_ip(src, dst)
-        paths = set(map(tuple, paths))
-        return json({"paths": paths, "status": "ok"}, dumps=dumps)
+        list_path = []
+        for path in paths:
+            set_path = {
+                'route_id' : path[0]+","+('0.0.0.0' if len(path)>=2 else path[1])+","+path[-1],
+                'start_node' : path[0],
+                'nexthop_node' : '0.0.0.0' if len(path)>=2 else path[1],
+                'end_node' : path[-1],
+                'path' : path
+            }
+            list_path.append(set_path)
+        return json({"paths": list_path, "status": "ok"}, dumps=dumps)
