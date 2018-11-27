d34f819902f478e0f7989dcfc25140733f5e6a71
Chayut Sawang <43782805+ibosszzz@users.noreply.github.com>
2018-11-28 04:46:56 +0700

    call api by name

--- backend/src/api/v1/routing.py
+++ backend/src/api/v1/routing.py
@@ -5,8 +5,11 @@ from sanic.views import HTTPMethodView
 
 class RoutingView(HTTPMethodView):
 
-    def get(self, request, device_id):
+    def get(self, request, device_name):
+        device_repo = request.app.db['device']
         cr_repo = request.app.db['copied_route']
-        routes = cr_repo.get_by_device_id(device_id)
+        _id = device_repo.get_oid_by_name(device_name)
+        print(_id[0]['_id'])
+        routes = cr_repo.get_by_device_id(_id[0]['_id'])
         # flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
         return json({"routes": routes, "status": "ok"}, dumps=dumps)
