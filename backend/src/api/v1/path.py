d19296cfb32435cd8616ac43e82ebd7cfa63e334
Chayut Sawang <43782805+ibosszzz@users.noreply.github.com>
2018-11-28 10:49:15 +0700

    error condition

--- backend/src/api/v1/path.py
+++ backend/src/api/v1/path.py
@@ -14,9 +14,9 @@ class PathView(HTTPMethodView):
         list_path = []
         for path in paths:
             set_path = {
-                'route_id' : path[0]+","+('0.0.0.0' if len(path)>=2 else path[1])+","+path[-1],
+                'route_id' : path[0]+","+('0.0.0.0' if len(path)<=2 else path[1])+","+path[-1],
                 'start_node' : path[0],
-                'nexthop_node' : '0.0.0.0' if len(path)>=2 else path[1],
+                'nexthop_node' : '0.0.0.0' if len(path)<=2 else path[1],
                 'end_node' : path[-1],
                 'path' : path
             }
