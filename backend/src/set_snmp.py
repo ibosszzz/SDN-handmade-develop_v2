8aac75fd43a60fcc77eb3738540a4f8e6fd582b3
Chayut Sawang <43782805+ibosszzz@users.noreply.github.com>
2019-03-22 18:31:28 +0700

    setup snmp

---
+++ backend/src/set_snmp.py
@@ -0,0 +1,35 @@
+import paramiko
+import time
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+devices = client.sdn01.device.find() #client.(database).(collection).find()
+
+def sleep():
+    while(remote_connect.recv(65535).decode("utf-8")[-1] not in "#>"):
+        time.sleep(0.1)
+
+for device in devices:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(device['management_ip'], port=22, username=device['ssh_info']['username'], password=device['ssh_info']['password'])
+    remote_connect = ssh.invoke_shell()
+    output = remote_connect.recv(65535)
+    print("connect to "+device['management_ip'], end=" ")
+    if output.decode("utf-8")[-1] == "#":
+        print("Privileged mode")
+    elif output.decode("utf-8")[-1] == ">":
+        print("User mode")
+        remote_connect.send("enable\n")
+        sleep()
+        remote_connect.send(input()+"\n")
+        sleep()
+    else:
+        pass
+    # set snmp
+    snmp_commands = ['conf t\n', 'snmp-server enable traps\n', 'snmp-server community public RO\n', 'snmp-server community private RW\n']
+    for command in snmp_commands:
+        remote_connect.send(command)
+        sleep()
+    
+    ssh.close()
