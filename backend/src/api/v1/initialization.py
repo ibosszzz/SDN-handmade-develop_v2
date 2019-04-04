a8b9221da2b616f810e428b8f7b103457f282b00
Chayut Sawang <43782805+ibosszzz@users.noreply.github.com>
2019-04-05 02:44:06 +0700

    create initialization.py

---
+++ backend/src/api/v1/initialization.py
@@ -0,0 +1,79 @@
+import paramiko
+import time
+
+from bson.json_util import dumps
+from sanic.response import json
+from sanic.views import HTTPMethodView
+
+from repository import DeviceRepository
+
+class InitializationView(HTTPMethodView):
+    def get(self, request):
+        return json({"success": True, "message": "Test"})
+
+    def install(self, request):
+        device_repo = request.app.db['device']
+        devices = device_repo.get_all()
+
+        for device in devices:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(device['management_ip'], port=22, username=device['ssh_info']['username'], password=device['ssh_info']['password'])
+            remote_connect = ssh.invoke_shell()
+            output = remote_connect.recv(65535)
+            if output.decode("utf-8")[-1] == "#":
+                #print("Privileged mode")
+            elif output.decode("utf-8")[-1] == ">":
+                #print("User mode")
+                remote_connect.send("enable\n")
+                time.sleep(0.5)
+                remote_connect.send(device['ssh_info']['secret']+"\n")
+                time.sleep(0.5)
+            else:
+                pass
+            # set snmp
+            snmp_commands = ['conf t\n', 'snmp-server enable traps\n', 'snmp-server community public RO\n', 'snmp-server community private RW\n']
+            for command in snmp_commands:
+                remote_connect.send(command)
+                time.sleep(0.5)
+            
+            ssh.close()
+
+        time.sleep(30)
+
+        for device in devices:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(device['management_ip'], port=22, username=device['ssh_info']['username'], password=device['ssh_info']['password'])
+            remote_connect = ssh.invoke_shell()
+            output = remote_connect.recv(65535)
+            if output.decode("utf-8")[-1] == "#":
+                #print("Privileged mode")
+            elif output.decode("utf-8")[-1] == ">":
+                #print("User mode")
+                remote_connect.send("enable\n")
+                time.sleep(0.5)
+                remote_connect.send(device['ssh_info']['secret']+"\n")
+                time.sleep(0.5)
+            else:
+                pass
+            # set netflow
+            interfaces = device_repo.get_interface(device['management_ip'])
+            remote_connect.send('conf t\n')
+            time.sleep(0.5)
+            for interface in interfaces:
+                for iface in interface['interfaces']:
+                    if "ipv4_address" in iface:
+                        for command in ['interface '+iface["description"]+'\n', 'ip policy route-map SDN-handmade\n', 'ip route-cache flow\n', 'exit\n']:
+                            remote_connect.send(command)
+                            time.sleep(0.5)
+            ip = request.json['management_ip'] #ip management device
+            port = '23456'
+            for command in ['ip flow-export destination '+ip+' '+port+'\n', 'ip flow-export version 9\n', 'ip flow-cache timeout active 1\n', 'ip flow-cache timeout inactive 15\n', 'ip flow-export template refresh-rate 1\n']:
+                remote_connect.send(command)
+                time.sleep(0.5)
+
+            ssh.close()
+
+        return json({"success": True, "message": "Initialization Success"})
+        
