228faa57e702fe33f37447dc9896057ba8ed66f9
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2021-09-20 19:28:02 +0700

    edit setting

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -3,20 +3,12 @@ from urllib.parse import quote
 database = {
     'default': {
         'driver': 'mongodb',  # Currently support only mongo
-        'uri': "mongodb://username:" + quote('password') + "@127.0.0.1:27017/",
-        'database': 'database_name',
+        'uri': "mongodb://127.0.0.1:27017",
+	#'uri': "mongodb://username:" + quote('password') + "@127.0.0.1:27017/",
+        'database': 'sdn01',
         'max_pool_size': 10,
     }
 }
-# database = {
-#     'default': {
-#         'driver': 'mongodb',  # Currently support only mongo
-#         'uri': "mongodb://127.0.0.1:27017",
-# 	#'uri': "mongodb://username:" + quote('password') + "@127.0.0.1:27017/",
-#         'database': 'sdn01',
-#         'max_pool_size': 10,
-#     }
-# }
 
 rest_api = {
     'host': '0.0.0.0',
